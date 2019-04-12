054b7e5e3d6dc7c10f4c49306ff79aa8946eba41
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-04-11 20:41:17 -0700

    added more titles and objects

--- src/components/portfolio/portfolio_container.js
+++ src/components/portfolio/portfolio_container.js
@@ -2,36 +2,34 @@ import React, { Component } from "react";
 
 import PortfolioItem from "./portfolio-item";
 
-
 export default class PortfolioContainer extends Component {
-    constructor(){
-        super();
-        console.log('container has rendered')
-    }
+  constructor() {
+    super();
 
-    portfolioItems() {
-        const data = ["Quip", "Eventbrite", "Ministry Safe"];
-    
-        return data.map(item => {
-            return <PortfolioItem title = {item} url = {"google.com"} />;
-        })
-    }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        { title: "Quip" },
+        { title: "Eventbrite" },
+        { title: "Ministry Safe" },
+        { title: "SwingAway" }
+      ]
+    };
+  }
 
-    render() {
-        return (
-            <div>
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
 
-               
-                <h2>Portfolio items go here...</h2>
-                
-                {this.portfolioItems()}
-               
-                
-            </div>
-        );
-    }
-}
+  render() {
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-function newFunction() {
-    return <portfolioItem />;
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
