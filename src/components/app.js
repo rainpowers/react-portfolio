300e466b457b93790439c959fb0b7ee2629fcaae
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-04-12 20:28:37 -0700

    adding buttons

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 import PortfolioContainer from './portfolio/portfolio_container';
+import NavigationContainer from './navigation/navigation-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
         <div>
---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+export default class NavigationComponent extends Component {
+    constructor() {
+        super();
+    }
+
+    AdminLinks() {
+
+    }
+
+    render(){
+        return (
+            <div>
+                <button>Home</button>
+                <button>About</button>
+                <button>Contact</button>
+                <button>Blog</button>
+                {true ? <button>Add Blog</button> : null}git
+            </div>
+        )
+    }
+
+}
